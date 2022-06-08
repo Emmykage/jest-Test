fda446cae0a95564ece93f61fa61392482835957
menanya <emmymenz@gmail.com>
2022-06-08 20:42:37 +0100

    finish task

--- string.test.js
+++ string.test.js
@@ -1,6 +1,6 @@
 const reverse = require('./index');
 
 it('string is reversed', () => {
-  expect(stringReverse('string')).toBe(stringReverse('string'));
+  expect(reverse('string')).toBe(reverse('string'));
 });
 
